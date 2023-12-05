6fdbe3d64afe4e3b32b98bb55794842fbe99cd40
Chris <chrislewis.davis@gmail.com>
2023-12-05 08:29:23 -0500

    exercise five

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -7,7 +7,28 @@ import styles from './ToastShelf.module.css';
 function ToastShelf() {
 
   // get our 'toast' state from the provider, useContext hook
-  const { toasts } = React.useContext(ToastContext);
+  const { toasts, setToasts, dismissToast } = React.useContext(ToastContext);
+
+  // adding a useEffect
+  React.useEffect(() => {
+    console.log('toast shelf mounted');
+
+    // create the keyDown function
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') {
+        console.log('keydown');
+        dismissToast(toasts.id); //not working
+      }
+    }
+
+    // create a listener on mount
+    window.addEventListener('keydown', handleKeyDown);
+
+    // cleanup
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toasts]);
 
   return (
     <ol className={styles.wrapper}>
